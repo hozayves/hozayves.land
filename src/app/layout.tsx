9dd11b939697b1a279d097a9a4f72dc927ce1ae1
hozayves <muhozayves2014@gmail.com>
2025-03-03 12:34:03 +0200

    add smoth scrolling props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body className={`${inter.className} relative`}>
         <main className="min-h-screen max-h-auto">
           {children}
